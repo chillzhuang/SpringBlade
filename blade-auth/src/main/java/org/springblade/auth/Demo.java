e63fc36752148320400d638a42bd8e70a21e364d
liulinkun <yoyo3287258@yeah.net>
2020-03-01 20:38:00 +0800

    git hub commit test by idea

---
+++ blade-auth/src/main/java/org/springblade/auth/Demo.java
@@ -0,0 +1,11 @@
+package org.springblade.auth;
+
+public class Demo {
+	private String filed1;
+
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+}
---
+++ blade-auth/src/main/java/org/springblade/auth/Demo2.java
@@ -0,0 +1,9 @@
+package org.springblade.auth;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		String var = "hello world";
+		System.out.println(var);
+	}
+}
